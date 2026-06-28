141f7ab8e0a30a48dd87d09f7cf526d6924f40a1
nahin889900 <ranusingh889900@gmail.com>
2026-06-28 23:28:02 +0600

    Create attendance_system.sql

---
+++ attendance_system.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE attendance_system;
+
+USE attendance_system;
+
+CREATE TABLE admin (
+
+id INT AUTO_INCREMENT PRIMARY KEY,
+
+username VARCHAR(50) NOT NULL,
+
+password VARCHAR(255) NOT NULL
+
+);
